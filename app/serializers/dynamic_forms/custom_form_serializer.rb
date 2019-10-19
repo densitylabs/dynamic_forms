98869f4b391487062e33cfc73b50b9c4e5631eb9
Alejandro Prado <isc.alejandro.prado@gmail.com>
2019-10-18 19:16:18 -0500

    include ActionModelSerializer to baseController (#28)

--- app/serializers/dynamic_forms/custom_form_serializer.rb
+++ app/serializers/dynamic_forms/custom_form_serializer.rb
@@ -1,6 +1,7 @@
 module DynamicForms
   class CustomFormSerializer < ActiveModel::Serializer
     attributes :json_schema, :ui_schema
+
     def json_schema
       return 'Not available' unless object.json_schema_available?
 
